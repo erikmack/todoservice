7ee4d9a7a2bd7e5a636682baa9e93ec03dd27eda
Erik Mackdanz <erikmack@gmail.com>
2010-09-01 23:01:45 -0500

    Initial commit

---
+++ src/resttest.c
@@ -0,0 +1,104 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include "credis.h"
+
+int main() {
+
+	char * err_format = 
+		"HTTP/1.1 %d %s\r\n"
+		"Content-Length: 0\r\n\r\n";
+
+	char * uri = getenv("REQUEST_URI");
+	char * method = getenv("REQUEST_METHOD");
+
+	if(!uri || !method) goto err400;
+	
+	REDIS rh;
+
+	rh = credis_connect("localhost", 6379, 2000);
+
+
+
+
+
+	// TODO: need to detect proxy use?
+	// That is, can REQUEST_URI come through as
+	// a full URL like http://cnn.com?  I think
+	// the web server filters these automtically,
+	// but it's worth testing since Apache logs
+	// show a lot of these
+
+	char * prefragment, * portion, * slug;
+
+	// Filter fragment.  User agents not allowed to send
+	// ... anyway, we'll just strip it off
+	prefragment = strtok_r( uri, "#", &uri );
+
+	// portions split before/after query string
+	portion = strtok_r( prefragment, "?", &prefragment );
+	if( portion ) { // means uri was non-blank
+
+		/*
+		int is_root = 1;
+		while((slug = strtok_r( portion, "/", &portion ))) {
+			is_root = 0;
+			found_slug( slug );	
+		}
+
+		char * pair, * key, * value;
+		while((pair = strtok_r( prefragment, "&", &prefragment ))) {
+			key = strtok_r( pair, "=", &pair );
+			value = strtok_r( pair, "=", &pair );
+			found_query_pair( key, value );
+		}
+		*/
+
+
+
+		int is_root = 1;
+		if((slug = strtok_r( portion, "/", &portion ))) {
+			is_root = 0;
+			if(!strcmp("version",slug) ) {
+
+				if(method && !strcmp("GET",method)) {
+					char * val;
+					credis_get(rh, "version", &val);
+
+					char * response = 
+						"HTTP/1.1 200 OK\r\n"
+						"Content-Type: text/javascript\r\n"
+						"Content-Length: %d\r\n\r\n"
+						"API.VersionReported(\"%s\");";
+
+					printf(response, strlen(val)+24, val);
+				} else goto err405;
+			//} else if(uri && !strcmp("/version",uri) ) {
+			} else goto err404;
+		}
+
+		if(is_root) {
+			// Handle request for root path /
+		}
+	}
+	
+
+
+
+	credis_close(rh);
+
+	return 0;
+
+	err400:
+		printf(err_format,400,"Bad request");
+		return 1;
+
+	err404:
+		printf(err_format,404,"File not found");
+		return 1;
+
+	err405:
+		printf(err_format,405,"Method not allowed");
+		return 1;
+}
